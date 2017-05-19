4654220a4bbb3485367811f65b5b749eb2b47a02
ANTHONY PRAJWAL P <anthonyprajwal11@gmail.com>
2017-05-19 10:35:21 +0530

    Add files via upload
    Instructions to run the lex programs:
    $lex program_name.l
    $gcc lex.yy.c -ll
    $./a.out

---
+++ data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* This is a program on Hello World
+ * This is by Anthony
+ *//*
+   This is a second comment */
+ 
+int main(){
+	printf("Hello World\n");
+	
+	/* The program says hello 
+	   // comment within a comment
+	*/	
+}
